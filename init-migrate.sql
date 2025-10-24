6332b2f3d3e2a159c02d7285dd6ecc5f2ac9b35e
danielsouzza <daniel.lima.souzza@gmail.com>
2025-10-24 12:49:34 -0300

    migrate

--- init-migrate.sql
+++ init-migrate.sql
@@ -1,3 +1,76 @@
+-- ========================================
+-- CRIAÇÃO DE USUÁRIO E PERMISSÕES
+-- ========================================
+
+-- Criar login no SQL Server (nível de servidor)
+IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'usuario_fideliza')
+BEGIN
+    CREATE LOGIN usuario_fideliza WITH PASSWORD = 'SenhaSegura123!';
+    PRINT 'Login usuario_fideliza criado com sucesso!';
+END
+ELSE
+BEGIN
+    PRINT 'Login usuario_fideliza já existe.';
+END
+GO
+
+-- Configurar permissões para BancoSammi
+USE BancoSammi;
+GO
+
+-- Criar usuário no banco BancoSammi
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'usuario_fideliza')
+BEGIN
+    CREATE USER usuario_fideliza FOR LOGIN usuario_fideliza;
+    PRINT 'Usuário usuario_fideliza criado no BancoSammi!';
+END
+ELSE
+BEGIN
+    PRINT 'Usuário usuario_fideliza já existe no BancoSammi.';
+END
+GO
+
+-- Conceder permissões no BancoSammi
+ALTER ROLE db_datareader ADD MEMBER usuario_fideliza;
+ALTER ROLE db_datawriter ADD MEMBER usuario_fideliza;
+PRINT 'Permissões concedidas ao usuario_fideliza no BancoSammi!';
+GO
+
+-- Configurar permissões para BancoFideliza
+USE BancoFideliza;
+GO
+
+-- Criar usuário no banco BancoFideliza
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'usuario_fideliza')
+BEGIN
+    CREATE USER usuario_fideliza FOR LOGIN usuario_fideliza;
+    PRINT 'Usuário usuario_fideliza criado no BancoFideliza!';
+END
+ELSE
+BEGIN
+    PRINT 'Usuário usuario_fideliza já existe no BancoFideliza.';
+END
+GO
+
+-- Conceder permissões no BancoFideliza
+ALTER ROLE db_datareader ADD MEMBER usuario_fideliza;
+ALTER ROLE db_datawriter ADD MEMBER usuario_fideliza;
+PRINT 'Permissões concedidas ao usuario_fideliza no BancoFideliza!';
+GO
+
+PRINT '========================================';
+PRINT 'CONFIGURAÇÃO DE USUÁRIO CONCLUÍDA';
+PRINT 'Login: usuario_fideliza';
+PRINT 'Senha: SenhaSegura123!';
+PRINT 'Bancos com acesso: BancoSammi, BancoFideliza';
+PRINT 'Permissões: Leitura e Escrita';
+PRINT '========================================';
+GO
+
+
+USE BancoFideliza;
+GO
+
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'NotasUsadas')
 BEGIN
     CREATE TABLE NotasUsadas (
@@ -55,3 +128,5 @@ BEGIN
     PRINT 'Tabela PontuacaoPendente já existe.';
 END
 GO
+
+
